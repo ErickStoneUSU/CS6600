9cdce373c5b390cbef13febb029bf9ffdd17cfd3
ErickStone <erick.stone@usu.edu>
2019-10-08 10:42:02 -0600

    more changes

--- Sources/hw06/mnist_convnet/mnist_convnet_2.py
+++ Sources/hw06/mnist_convnet/mnist_convnet_2.py
@@ -5,63 +5,28 @@
 # bugs to vladimir dot kulyukin at usu dot edu
 ################################################
 
-# uncomment if in Py2.
-# from __future__ import division, print_function
-
 import tflearn
-from tflearn.data_utils import shuffle, to_categorical
-from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, fully_connected
 from tflearn.layers.estimator import regression
-import pickle
-import tflearn.datasets.mnist as mnist
+from hw06.mnist_convnet.test_mnist_convnets import init
 
-# X is the training data set; Y is the labels for X.
-# testX is the testing data set; testY is the labels for testX.
-X, Y, testX, testY = mnist.load_data(one_hot=True)
-X, Y = shuffle(X, Y)
-testX, testY = shuffle(testX, testY)
-trainX = X[0:50000]
-trainY = Y[0:50000]
-validX = X[50000:]
-validY = Y[50000:]
-# make sure you reshape the training and testing
-# data as follows.
-trainX = trainX.reshape([-1, 28, 28, 1])
-testX  = testX.reshape([-1, 28, 28, 1])
 
-def save(obj, file_name):
-    with open(file_name, 'wb') as fp:
-        pickle.dump(obj, fp)
+def components_2():
+    input_layer = input_data(shape=[None, 28, 28, 1])
+    conv_layer_1 = conv_2d(input_layer, nb_filter=20, filter_size=5, activation='relu', name='conv_layer_1')
+    pool_layer_1 = max_pool_2d(conv_layer_1, 2, name='pool_layer_1')
+    conv_layer_2 = conv_2d(pool_layer_1, nb_filter=40, filter_size=3, activation='relu', name='conv_layer_2')
+    pool_layer_2 = max_pool_2d(conv_layer_2, 2, name='pool_layer_2')
+    fc_layer_1 = fully_connected(pool_layer_2, 100, activation='relu', name='fc_layer_1')
+    fc_layer_2 = fully_connected(fc_layer_1, 10, activation='softmax', name='fc_layer_2')
+    return fc_layer_2
 
-# change these two paths accordingly.
-VALID_X_PATH = '/home/vladimir/research/mnist_convnets/valid_x.pck'
-VALID_Y_PATH = '/home/vladimir/research/mnist_convnets/valid_y.pck'
-save(validX, VALID_X_PATH)
-save(validY, VALID_Y_PATH)
 
-def build_tflearn_convnet_2(): 
-    # your code here.
+def build_tflearn_convnet_2():
+    network = regression(components_2(), optimizer='sgd', loss='categorical_crossentropy', learning_rate=0.01)
+    model = tflearn.DNN(network)
+    return model
 
-# the model is trained for a specified NUM_EPOCHS
-# with a specified batch size; of course, you'll want
-# to raise the number of epochs to some larger number.
-NUM_EPOCHS = 30
-BATCH_SIZE = 10
-MODEL = build_tflearn_convnet_2()
-MODEL.fit(trainX, trainY, n_epoch=NUM_EPOCHS,
-          shuffle=True,
-          validation_set=(testX, testY),
-          show_metric=True,
-          batch_size=BATCH_SIZE,
-          run_id='MNIST_ConvNet_2')
 
-# let me raise my text size at you: DO NOT FORGET TO PERSIST
-# YOUR TRAINED MODELS. THIS IS AN IMPORTANT COMMAND.
-SAVE_CONVNET_PATH = '/home/vladimir/research/mnist_convnets/hw06_my_net_02.tfl'
-MODEL.save(SAVE_CONVNET_PATH)
-
-# this is just to make sure that you've trained everything
-# correctly.
-#print(model.predict(testX[0].reshape([-1, 28, 28, 1])))
-
+init(build=build_tflearn_convnet_2, id='MNIST_ConvNet_2', num='2')
--- Sources/hw06/mnist_convnet/test_mnist_convnets.py
+++ Sources/hw06/mnist_convnet/test_mnist_convnets.py
@@ -1,93 +1,50 @@
-
-## uncomment this import if you use Py2.
-## from __future__ import division, print_function
-
 ################################################
 # module: mnist_convnet_load.py
 # bugs to vladimir dot kulyukin at usu dot edu
 ################################################
 
-import tflearn
-from tflearn.data_utils import shuffle, to_categorical
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.estimator import regression
-import tflearn.datasets.mnist as mnist
-import numpy as np
 import pickle
+
+import numpy as np
+import tflearn
+import tflearn.datasets.mnist as mnist
 from scipy import stats
+from tflearn.data_utils import shuffle
 
-# uncomment and change these paths accordingly
-net_path_1 = '/home/vladimir/research/mnist_convnets/hw06_my_net_01.tfl'
-#net_path_2 = '/home/vladimir/research/mnist_convnets/hw06_my_net_02.tfl'
-#net_path_3 = '/home/vladimir/research/mnist_convnets/hw06_my_net_03.tfl'
-#net_path_4 = '/home/vladimir/research/mnist_convnets/hw06_my_net_04.tfl'
-#net_path_5 = '/home/vladimir/research/mnist_convnets/hw06_my_net_05.tfl'
+from hw06.mnist_convnet.mnist_convnet_1 import components_1
+from hw06.mnist_convnet.mnist_convnet_2 import components_2
+from hw06.mnist_convnet.mnist_convnet_3 import components_3
+from hw06.mnist_convnet.mnist_convnet_4 import components_4
+from hw06.mnist_convnet.mnist_convnet_5 import components_5
 
-def load_mnist_convnet_1(path):
-    input_layer = input_data(shape=[None, 28, 28, 1])
-    conv_layer_1  = conv_2d(input_layer,
-                            nb_filter=20,
-                            filter_size=5,
-                            activation='relu',
-                            name='conv_layer_1')
-    pool_layer_1  = max_pool_2d(conv_layer_1, 2,
-                                name='pool_layer_1')
-    conv_layer_2 = conv_2d(pool_layer_1,
-                           nb_filter=40,
-                           filter_size=3,
-                           activation='relu',
-                           name='conv_layer_2')
-    pool_layer_2 = max_pool_2d(conv_layer_2, 2,
-                               name='pool_layer_2')
-    fc_layer_1  = fully_connected(pool_layer_2, 100,
-                                  activation='relu',
-                                  name='fc_layer_1')
-    fc_layer_2 = fully_connected(fc_layer_1, 10,
-                                 activation='softmax',
-                                 name='fc_layer_2')
-    model = tflearn.DNN(fc_layer_2)
-    # load the trained and persisted network.
+
+def load_mnist_convnet_1(path): return get_model(components_1, path)
+def load_mnist_convnet_2(path): return get_model(components_2, path)
+def load_mnist_convnet_3(path): return get_model(components_3, path)
+def load_mnist_convnet_4(path): return get_model(components_4, path)
+def load_mnist_convnet_5(path): return get_model(components_5, path)
+def get_model(components, path):
+    model = tflearn.DNN(components())
     model.load(path)
     return model
 
-def load_mnist_convnet_2(path):
-    # your code here.
-
-def load_mnist_convnet_3(path):
-    # your code here.
-
-def load_mnist_convnet_4(path):
-    # your code here.
-
-def load_mnist_convnet_5(path):
-    # your code
-
-net_model_1 = load_mnist_convnet_1(net_path_1)
-#net_model_2 = load_mnist_convnet_2(net_path_2)
-#net_model_3 = load_mnist_convnet_3(net_path_3)
-#net_model_4 = load_mnist_convnet_4(net_path_4)
-#net_model_5 = load_mnist_convnet_5(net_path_5)
 
 def load(file_name):
     with open(file_name, 'rb') as fp:
         obj = pickle.load(fp)
     return obj
+def save(obj, file_name):
+    with open(file_name, 'wb') as fp:
+        pickle.dump(obj, fp)
 
-# load the validation data; change these directories accordingly
-# change these two paths accordingly.
-VALID_X_PATH = '/home/vladimir/research/mnist_convnets/valid_x.pck'
-VALID_Y_PATH = '/home/vladimir/research/mnist_convnets/valid_y.pck'
-validX = load(VALID_X_PATH)
-validY = load(VALID_Y_PATH)
 
 def test_tflearn_convnet_model(convnet_model, validX, validY):
     results = []
     for i in range(len(validX)):
         prediction = convnet_model.predict(validX[i].reshape([-1, 28, 28, 1]))
-        results.append(np.argmax(prediction, axis=1)[0] == \
-                       np.argmax(validY[i]))
-    return sum((np.array(results) == True))/len(results)
+        results.append(np.argmax(prediction, axis=1)[0] == np.argmax(validY[i]))
+    return sum((np.array(results) == True)) / len(results)
+
 
 def run_convnet_ensemble(net_ensemble, sample):
     test_results = []
@@ -96,6 +53,7 @@ def run_convnet_ensemble(net_ensemble, sample):
         test_results.append(np.argmax(pred, axis=1)[0])
     return stats.mode(test_results).mode[0]
 
+
 def evaluate_convnet_ensemble(net_ensemble, validX, validY):
     assert len(validX) == len(validY)
     test_results = []
@@ -103,18 +61,62 @@ def evaluate_convnet_ensemble(net_ensemble, validX, validY):
         test_results.append(run_convnet_ensemble(net_ensemble, x), y)
     return sum(int(x == y) for x, y in test_results), len(validX)
 
-if __name__ == '__main__':
-    print('ConvNet 1 accuracy = {}'.format(test_tflearn_convnet_model(net_model_1, validX, validY)))
 
-# let's test the persisted network on 20
-# randomly selected validation samples.
-#tests = []
-#num_tests = 20
-#for j in xrange(num_tests):
-#    i = np.random.randint(0, len(validX)-1)
-#    prediction = model.predict(validX[i].reshape([-1, 28, 28, 1]))
-#
-    # print(np.argmax(prediction, axis=1)[0] == np.argmax(validY[i]))
-#    tests.append(np.argmax(prediction, axis=1)[0] == np.argmax(validY[i]))
-#
-#print(sum((np.array(tests) == True))/num_tests)
+def get_data():
+    x, y, test_x, test_y = mnist.load_data(one_hot=True)
+    x, y = shuffle(x, y)
+    test_x, test_y = shuffle(test_x, test_y)
+    train_x = x[0:50000]
+    train_y = y[0:50000]
+    valid_x = x[50000:]
+    valid_y = y[50000:]
+    # make sure you reshape the training and testing
+    # data as follows.
+    train_x = train_x.reshape([-1, 28, 28, 1])
+    test_x = test_x.reshape([-1, 28, 28, 1])
+    return train_x, train_y, test_x, test_y, valid_x, valid_y
+
+
+def init(build, id, num):
+    train_x, train_y, test_x, test_y, valid_x, valid_y = get_data()
+    save(valid_x, '/data/valid_x.pck')
+    save(valid_y, '/data/valid_y.pck')
+    NUM_EPOCHS = 2
+    BATCH_SIZE = 10
+    MODEL = build()
+    MODEL.fit(train_x, train_y, n_epoch=NUM_EPOCHS,
+              shuffle=True,
+              validation_set=(test_x, test_y),
+              show_metric=True,
+              batch_size=BATCH_SIZE,
+              run_id=id)
+    MODEL.save('/data/hw06_my_net_0' + num + '.tfl')
+    print(MODEL.predict(test_x[0].reshape([-1, 28, 28, 1])))
+
+
+def test_20(valid_x, valid_y, model, full=False):
+    tests = []
+    num_tests = 20
+    for j in range(num_tests):
+        i = np.random.randint(0, len(valid_x) - 1)
+        prediction = model.predict(valid_x[i].reshape([-1, 28, 28, 1]))
+        if full:
+            print(np.argmax(prediction, axis=1)[0] == np.argmax(valid_y[i]))
+        tests.append(np.argmax(prediction, axis=1)[0] == np.argmax(valid_y[i]))
+    print(sum((np.array(tests) == True)) / num_tests)
+
+
+def test_model(tfl, loader):
+    valid_x = load('/data/valid_x.pck')
+    valid_y = load('/data/valid_y.pck')
+    model = loader(tfl)
+    test_20(valid_x, valid_y, model, True)
+    # print('ConvNet '+num+' accuracy = {}'.format(test_tflearn_convnet_model(model, valid_x, valid_y)))
+
+
+if __name__ == '__main__':
+    test_model('/data/hw06_my_net_01.tfl', load_mnist_convnet_1)
+    # test_model('/data/hw06_my_net_02.tfl', load_mnist_convnet_2)
+    # test_model('/data/hw06_my_net_03.tfl', load_mnist_convnet_3)
+    # test_model('/data/hw06_my_net_04.tfl', load_mnist_convnet_4)
+    # test_model('/data/hw06_my_net_05.tfl', load_mnist_convnet_5)
